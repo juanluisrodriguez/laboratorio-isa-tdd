9ec8f4e0eb8ddd8397e1d94619a6925918e4efc3
Juan Luis <webmaster@memoriasdeunaprendiz.com>
2019-05-15 22:02:48 +0200

    Test: al sumar -5 más 8 devuelve 3

--- tests/unit/Calculadora.spec.js
+++ tests/unit/Calculadora.spec.js
@@ -1,29 +1,42 @@
 import Calculadora from '@/services/Calculadora.js'
 
 describe('Calculadora', () => {
-  it('al sumar 2 más 3 devuelve 5', () => {
-    //Arrange
-    const firstNumber = 2;
-    const secondNumber = 3;
-    const expectedResult = 5;
+    it('al sumar 2 más 3 devuelve 5', () => {
+        //Arrange
+        const firstNumber = 2;
+        const secondNumber = 3;
+        const expectedResult = 5;
 
-    //Act
-    const result = Calculadora.add(firstNumber,secondNumber);
+        //Act
+        const result = Calculadora.add(firstNumber, secondNumber);
 
-    //Assert
-    expect(result).toBe(expectedResult);
-  });
+        //Assert
+        expect(result).toBe(expectedResult);
+    });
 
-  it('al sumar 6 más 8 devuelve 14', () => {
-    //Arrange
-    const firstNumber = 6;
-    const secondNumber = 8;
-    const expectedResult = 14;
+    it('al sumar 6 más 8 devuelve 14', () => {
+        //Arrange
+        const firstNumber = 6;
+        const secondNumber = 8;
+        const expectedResult = 14;
 
-    //Act
-    const result = Calculadora.add(firstNumber,secondNumber);
+        //Act
+        const result = Calculadora.add(firstNumber, secondNumber);
 
-    //Assert
-    expect(result).toBe(expectedResult);
-  })
+        //Assert
+        expect(result).toBe(expectedResult);
+    });
+
+    it('al sumar -5 más 8 devuelve 3', () => {
+        //Arrange
+        const firstNumber = -5;
+        const secondNumber = 8;
+        const expectedResult = 3;
+
+        //Act
+        const result = Calculadora.add(firstNumber, secondNumber);
+
+        //Assert
+        expect(result).toBe(expectedResult);
+    })
 });
