565f9db6b774a3bf2ce0f9f1c544931b9d45f756
Juan Luis <webmaster@memoriasdeunaprendiz.com>
2019-05-16 20:03:05 +0200

    Code: Si el segundo parámetro es un string Numérico devuelve NaN

--- src/services/Calculadora.js
+++ src/services/Calculadora.js
@@ -1,7 +1,7 @@
 export default {
     add(first, second){
         if (!Number.isInteger(first)) return NaN;
-        if (second=='a') return NaN;
+        if (!Number.isInteger(second)) return NaN;
 
         return first + second;
     }
