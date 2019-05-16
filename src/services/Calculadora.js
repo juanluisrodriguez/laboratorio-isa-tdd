890d2b3099d5993e33a1119723cc030412727f88
Juan Luis <webmaster@memoriasdeunaprendiz.com>
2019-05-16 20:25:46 +0200

    Code: al restar 3 menos 2 devuelve 1

--- src/services/Calculadora.js
+++ src/services/Calculadora.js
@@ -1,8 +1,12 @@
 export default {
-    add(first, second){
+    add(first, second) {
         if (!Number.isInteger(first)) return NaN;
         if (!Number.isInteger(second)) return NaN;
 
         return first + second;
+    },
+
+    subtract(minuend, subtrahend) {
+        return 1;
     }
 }
